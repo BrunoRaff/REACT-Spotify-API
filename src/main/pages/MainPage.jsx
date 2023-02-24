9e057100d5ead7a253fc0a8f5ad6ce7d5ab298b2
Luis Canales <luisecanales48@gmail.com>
2023-02-23 23:40:15 -0500

    top tracks

--- src/main/pages/MainPage.jsx
+++ src/main/pages/MainPage.jsx
@@ -8,18 +8,18 @@ import './MainPage.css'
 export const MainPage = ({ code }) => {
 
   const [token, setToken] = useState('');
-  const [artists, setArtists] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
   const getAccessData = async () => {
     const { access_token } = await getToken(code);
     setToken(access_token);
   }
 
-  const getUserTopArtists = async () => {
+  const getUserTopTracks = async () => {
     if (!token) return;
-    const topArtists = await getUserTop('artists', token, 'short_term', 6);
-    const { items } = topArtists;
-    setArtists(items);
+    const topTracks = await getUserTop('tracks', token, 'short_term', 6);
+    const { items } = topTracks;
+    setTracks(items);
   }
 
   useEffect(() => {
@@ -27,26 +27,12 @@ export const MainPage = ({ code }) => {
   }, [])
 
   useEffect(() => {
-    getUserTopArtists();
+    getUserTopTracks();
   }, [token])
-
-  async function userRa(){
-   const token = await getToken(code);
-   console.log(token);
-   const userGa = await getUser(token.access_token);
-   setUser(userGa);
-  }
-
-  useEffect(() => {
-    
-    userRa();
-    
-  }, [])
-  
 
   return (
     <main>
-      <CardContainer artists={artists} />
+      <CardContainer tracks={tracks} />
     </main >
   )
 }
